b9cd8288b9f52e73314b71dabee643bcacb10b07
Hagfaner <118606538+Hagfaner@users.noreply.github.com>
2023-05-23 19:32:29 +0400

    функционал

--- lab8/MainWindow.xaml.cs
+++ lab8/MainWindow.xaml.cs
@@ -21,16 +21,124 @@ namespace lab8
     public partial class MainWindow : Window
     {
         Standart exp = new Standart();
+        static char oper =' ';
+        static bool dec_point;
+        static bool postequil = false;
         public MainWindow()
         {
             
             InitializeComponent();
-           
+            display.Text = "0";
+        }
+
+       
+
+        private void place_number(object sender, RoutedEventArgs e)
+        {
+            if (postequil)
+            {
+                sec_display.Text ="" ;
+                display.Text = "0";
+                postequil = false;
+            }
+            string buf=sender.ToString();
+            char num = buf[buf.Length - 1];
+            if (display.Text[0] == '0' && !dec_point)
+            {
+                display.Text=display.Text.Substring(1);
+            }
+            display.Text = exp.place_number(num, display.Text);
+        }
+
+        private void clear__Click(object sender, RoutedEventArgs e)
+        {
+            display.Text = "0";
+            dec_point = false;
+            dec.IsEnabled = true;
+        }
+
+        private void clear_all_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text = "0";
+            sec_display.Text = "";
+            dec_point = false;
+            dec.IsEnabled = true;
+        }
+
+        private void operations_click(object sender, RoutedEventArgs e)
+        {
+            postequil = false;
+            oper = exp.operations(sender);
+            sec_display.Text = display.Text +' '+oper+' ';
+            dec_point = false;
+            dec.IsEnabled = true;
+            display.Text = "0";
         }
 
         private void equils_Click(object sender, RoutedEventArgs e)
         {
-            display.Text=exp.calculation(display.Text);
+           if(oper!=' ')
+            {
+                display.Text = exp.calculation(sec_display.Text + display.Text, oper);
+                sec_display.Text = "";
+                oper = ' ';
+                dec_point = false;
+                dec.IsEnabled = true;
+                postequil = true;
+            }
+           
+            
+        }
+
+        private void dec_Click(object sender, RoutedEventArgs e)
+        {
+            dec_point = true;
+            display.Text += ',';
+            dec.IsEnabled = false;
+        }
+
+        private void one_div_x_Click(object sender, RoutedEventArgs e)
+        {
+            sec_display.Text = "1/" + display.Text +"=";
+            display.Text = exp.div1x(display.Text);
+            postequil = true;
+        }
+
+        private void neg_Click(object sender, RoutedEventArgs e)
+        {
+            display.Text = exp.negativ(display.Text);
+            postequil = true;
+        }
+
+        private void sqrt_x_Click(object sender, RoutedEventArgs e)
+        {
+            sec_display.Text = "sqrt(" + display.Text + ")=";
+            display.Text = exp.sqrtx(display.Text);
+            postequil = true;
+        }
+        private void sqr_x_Click(object sender, RoutedEventArgs e)
+        {
+            sec_display.Text = display.Text + "^2 =";
+            display.Text = exp.pow_2(display.Text);
+            postequil = true;
+        }
+      
+        private void powof2_Click(object sender, RoutedEventArgs e)
+        {
+            sec_display.Text = "2^"+display.Text+"=";
+            display.Text = exp.powof2(display.Text);
+            postequil = true;
+        }
+
+        private void clear_last_number_Click(object sender, RoutedEventArgs e)
+        {
+            if (display.Text.Length <= 1)
+            {
+                display.Text = "0";
+            }
+            else { 
+            display.Text = display.Text.Remove(display.Text.Length - 1);
+            }
         }
     }
 }
--- lab8/Standart.cs
+++ lab8/Standart.cs
@@ -9,10 +9,109 @@ namespace lab8
 {
     internal class Standart
     {
-
-        public string calculation(string expression)
+        public string place_number(char num,string display)
         {
-            return "";
+            
+            switch (num)
+            {
+               
+                case '0':
+                    display += "0";
+                    break;
+                case '1':
+                    display += "1";
+                    break;
+                case '2':
+                    display += "2";
+                    break;
+                case '3':
+                    display += "3";
+                    break;
+                case '4':
+                    display += "4";
+                    break;
+                case '5':
+                    display += "5";
+                    break;
+                case '6':
+                    display += "6";
+                    break;
+                case '7':
+                    display += "7";
+                    break;
+                case '8':
+                    display += "8";
+                    break;
+                case '9':
+                    display += "9";
+                    break;
+                default:
+                    break;
+            }
+            return display;
+        }
+        public char operations(object sender)
+        {
+            string buf = sender.ToString();
+            char oper = buf[buf.Length - 1];
+            return oper;
+        }
+        public string calculation(string expression, char operation)
+        {
+            double res = 0;
+            string[] buf;
+           switch(operation) {
+                case '+':
+                    buf=expression.Split(" + ");
+                    res = double.Parse(buf[0]) + double.Parse(buf[1]);
+                    break;
+                case '-':
+                    buf = expression.Split(" - ");
+                    res = double.Parse(buf[0]) - double.Parse(buf[1]);
+                    break;
+                case '/':
+                    buf = expression.Split(" / ");
+                    res = double.Parse(buf[0]) / double.Parse(buf[1]);
+                    break;
+                case '*':
+                    buf = expression.Split(" * ");
+                    res = double.Parse(buf[0]) * double.Parse(buf[1]);
+                    break;
+                default:
+                    break;
+            }
+            return res.ToString();
+        }
+        public string pow_2(string expression)
+        {
+            double x = double.Parse(expression);
+            return (x * x).ToString();
+        }
+        public string div1x(string expression)
+        {
+            double x = double.Parse(expression);
+            return (1.0/x).ToString();
+        }
+        public string sqrtx (string expression)
+        {
+            double x = double.Parse(expression);
+            return Math.Sqrt(x).ToString();
+        }
+        public string negativ(string expression) {
+            if (expression[0] != '-')
+            {
+                expression = '-' + expression;
+            }
+            else
+            {
+                expression=expression.Substring(1);
+            }
+            return expression;
+        }
+        public string powof2(string expression)
+        {
+            double x = double.Parse(expression);
+            return Math.Pow(2, x).ToString();
         }
     }
 }
